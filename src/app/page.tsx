fe614af7a324bdae2b3915c88443b9c60215c20a
yuto-kawahara <mashroom1384@gmail.com>
2025-01-11 13:42:26 +0900

    更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
+            はじめる
           </a>
         </div>
       </main>
@@ -73,7 +73,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          例
         </a>
         <a
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
