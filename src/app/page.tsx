8b0f5db6319ef626eabcacf3288ffc266bc8988b
yuto-kawahara <mashroom1384@gmail.com>
2025-01-18 14:37:43 +0900

    トップ画面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,35 @@
 "use client";
 import { FormEvent, useState } from "react";
-import ReCAPTCHA from "react-google-recaptcha";
 import Lottie from "react-lottie";
 import p_anime from "../../public/lottie/portfolio/p.json";
 import o_anime from "../../public/lottie/portfolio/o.json";
 import r_anime from "../../public/lottie/portfolio/r.json";
 import t_anime from "../../public/lottie/portfolio/t.json";
+import f_anime from "../../public/lottie/portfolio/f.json";
+import l_anime from "../../public/lottie/portfolio/l.json";
+import i_anime from "../../public/lottie/portfolio/i.json";
 
 export default function Home() {
-  const [recaptchaToken, setRecaptchaToken] = useState("");
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+  // const [recaptchaToken, setRecaptchaToken] = useState("");
+  // const handleSubmit = async (e: FormEvent) => {
+  //   e.preventDefault();
 
-    // reCAPTCHAトークンを送信
-    const res = await fetch("/api/verify-recaptcha", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ token: recaptchaToken }),
-    });
+  //   // reCAPTCHAトークンを送信
+  //   const res = await fetch("/api/verify-recaptcha", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ token: recaptchaToken }),
+  //   });
 
-    const result = await res.json();
-    if (result.success) {
-      alert("reCAPTCHA verification passed!");
-    } else {
-      alert("reCAPTCHA verification failed!");
-    }
-  };
+  //   const result = await res.json();
+  //   if (result.success) {
+  //     alert("reCAPTCHA verification passed!");
+  //   } else {
+  //     alert("reCAPTCHA verification failed!");
+  //   }
+  // };
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -52,6 +54,18 @@ export default function Home() {
     ...defaultOptions,
     animationData: t_anime,
   };
+  const fOptions = {
+    ...defaultOptions,
+    animationData: f_anime,
+  };
+  const lOptions = {
+    ...defaultOptions,
+    animationData: l_anime,
+  };
+  const iOptions = {
+    ...defaultOptions,
+    animationData: i_anime,
+  };
 
   return (
     <div>
@@ -59,31 +73,59 @@ export default function Home() {
         <div style={{ display: "flex", alignItems: "center" }}>
           <Lottie
             options={pOptions}
-            height={800}
-            width={250}
+            // height={800}
+            // width={250}
             isStopped={false}
             isPaused={false}
+
           />
           <Lottie
             options={oOptions}
-            height={800}
-            width={250}
+            // height={800}
+            // width={250}
             isStopped={false}
             isPaused={false}
+
           />
           <Lottie
             options={rOptions}
-            height={800}
-            width={250}
+            // height={800}
+            // width={250}
             isStopped={false}
             isPaused={false}
+
           />
           <Lottie
             options={tOptions}
-            height={800}
-            width={200}
+            // height={800}
+            // width={200}
             isStopped={false}
             isPaused={false}
+
+          />
+          <Lottie
+            options={fOptions}
+            // height={800}
+            // width={200}
+            isStopped={false}
+            isPaused={false}
+
+          />
+          <Lottie
+            options={lOptions}
+            // height={800}
+            // width={200}
+            isStopped={false}
+            isPaused={false}
+
+          />
+          <Lottie
+            options={iOptions}
+            // height={800}
+            // width={200}
+            isStopped={false}
+            isPaused={false}
+
           />
         </div>
 
